a7292296f33dd9c6f487c4d47d7a6bd85c62e302
Annabel Lim <annabellim2000@yahoo.com>
2026-03-05 05:52:38 -0500

    created xCode Project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Annabel Lim on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
