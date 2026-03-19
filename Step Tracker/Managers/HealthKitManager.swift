7025ae8a11176a2d1530b8696842276a06653fb8
Annabel Lim <annabellim2000@yahoo.com>
2026-03-19 11:37:20 -0400

    text input validation

--- Step Tracker/Managers/HealthKitManager.swift
+++ Step Tracker/Managers/HealthKitManager.swift
@@ -13,6 +13,8 @@ enum STError: LocalizedError {
     case sharingDenied(quantityType: String)
     case noData
     case unableToCompleteRequest
+    case invalidValue
+    
     
     var errorDescription: String? {
         switch self {
@@ -24,6 +26,8 @@ enum STError: LocalizedError {
             "No Data"
         case .unableToCompleteRequest:
             "Unable to Complete Request"
+        case .invalidValue:
+            "Invalid Value"
         }
     }
     
@@ -37,6 +41,8 @@ enum STError: LocalizedError {
             "There is no data for this Health statistic."
         case .unableToCompleteRequest:
             "We are unable to complete your request at this time.\n\nPlease try again later."
+        case .invalidValue:
+            "Must be a numeric value with a maximum of one decimal place."
         }
     }
 }
