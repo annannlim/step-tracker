aa91206126d2c5da5e7114d0b3c470bbac9a272c
Annabel Lim <annabellim2000@yahoo.com>
2026-03-24 08:44:25 -0400

    swift 6 concurrency

--- Step Tracker/Managers/HealthKitManager.swift
+++ Step Tracker/Managers/HealthKitManager.swift
@@ -9,14 +9,22 @@ import Foundation
 import HealthKit
 
 @Observable
-class HealthKitManager {
+@MainActor
+final class HealthKitData: Sendable {
+    var stepData: [HealthMetric] = []
+    var weightData: [HealthMetric] = []
+    var weightDiffData: [HealthMetric] = []
+}
+
+@Observable
+final class HealthKitManager: Sendable {
 
     let store = HKHealthStore()
     let types: Set = [HKQuantityType(.stepCount), HKQuantityType(.bodyMass)]
 
-    var stepData: [HealthMetric] = []
-    var weightData: [HealthMetric] = []
-    var weightDiffData: [HealthMetric] = []
+//    var stepData: [HealthMetric] = []
+//    var weightData: [HealthMetric] = []
+//    var weightDiffData: [HealthMetric] = []
     
     /// Fetch last 28 days of step count from HealthKit
     ///
@@ -144,25 +152,25 @@ class HealthKitManager {
     }
     
     
-    //    func addSimulatorData() async {
-    //
-    //        var mockSamples: [HKQuantitySample] = []
-    //
-    //        for i in 0..<28 {
-    //            let stepQuantity = HKQuantity(unit: .count(), doubleValue: .random(in: 4_000...20_000))
-    //            let weightQuantity = HKQuantity(unit: .pound(), doubleValue: .random(in: (160 + Double(i/3)...165 + Double(i/3) )))
-    //
-    //            let startDate = Calendar.current.date(byAdding: .day, value: -i, to: .now)!
-    //            let endDate = Calendar.current.date(byAdding: .second, value: 1, to: startDate)!
-    //
-    //            let stepSample = HKQuantitySample(type: HKQuantityType(.stepCount), quantity: stepQuantity, start: startDate, end: endDate)
-    //            let weightSample = HKQuantitySample(type: HKQuantityType(.bodyMass), quantity:weightQuantity, start: startDate, end: endDate)
-    //
-    //            mockSamples.append(stepSample)
-    //            mockSamples.append(weightSample)
-    //        }
-    //
-    //        try! await store.save(mockSamples)
-    //        print("TRACE Dummy Data sent up")
-    //    }
+//        func addSimulatorData() async {
+//    
+//            var mockSamples: [HKQuantitySample] = []
+//    
+//            for i in 0..<28 {
+//                let stepQuantity = HKQuantity(unit: .count(), doubleValue: .random(in: 4_000...20_000))
+//                let weightQuantity = HKQuantity(unit: .pound(), doubleValue: .random(in: (160 + Double(i/3)...165 + Double(i/3) )))
+//    
+//                let startDate = Calendar.current.date(byAdding: .day, value: -i, to: .now)!
+//                let endDate = Calendar.current.date(byAdding: .second, value: 1, to: startDate)!
+//    
+//                let stepSample = HKQuantitySample(type: HKQuantityType(.stepCount), quantity: stepQuantity, start: startDate, end: endDate)
+//                let weightSample = HKQuantitySample(type: HKQuantityType(.bodyMass), quantity:weightQuantity, start: startDate, end: endDate)
+//    
+//                mockSamples.append(stepSample)
+//                mockSamples.append(weightSample)
+//            }
+//    
+//            try! await store.save(mockSamples)
+//            print("TRACE Dummy Data sent up")
+//        }
 }
